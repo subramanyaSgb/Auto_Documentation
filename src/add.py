8954b41b1c4b9f5c1da1f071b351a99ae9687c53
subramanyaSgb <subramanyagbellary@gmail.com>
2025-03-12 15:43:19 +0530

    first commit

---
+++ src/add.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    """
+    Add two numbers.
+
+    Args:
+        a (int): First number
+        b (int): Second number
+
+    Returns:
+        int: Sum of a and b
+    """
+    return a + b
+
+if __name__ == "__main__":
+    result = add(1,2)
+    print(result)
